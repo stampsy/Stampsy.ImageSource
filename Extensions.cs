42b7bfe4e70ed7282881958471201b5f0b2ce2c2
Dan Abramov <dan.abramov@gmail.com>
2013-05-17 01:35:24 +0400

    Add extension method to call Fetch on destination instance, e.g. _myFolder.Fetch ('dropbox://myfile.png')

--- Extensions.cs
+++ Extensions.cs
@@ -4,21 +4,27 @@ using System.Threading.Tasks;
 
 namespace Stampsy.ImageSource
 {
-    internal static class Extensions
+    public static class Extensions
     {
-        public static IObservable<T> SurroundWith<T> (this IObservable<T> a, IObservable<T> b)
+        public static Task<TRequest> Fetch<TRequest> (this IDestination<TRequest> destination, Uri url)
+            where TRequest : Request
+        {
+            return ImageSource.Fetch (url, destination);
+        }
+
+        internal static IObservable<T> SurroundWith<T> (this IObservable<T> a, IObservable<T> b)
         {
             return b.Concat (a).Concat (b);
         }
 
-        public static void RouteExceptions<T> (this Task task, IObserver<T> observer)
+        internal static void RouteExceptions<T> (this Task task, IObserver<T> observer)
         {
             task.ContinueWith (t => {
                 observer.OnError (t.Exception.Flatten ());
             }, TaskContinuationOptions.OnlyOnFaulted);
         }
 
-        public static void RouteExceptions<T> (this Task task, TaskCompletionSource<T> tcs)
+        internal static void RouteExceptions<T> (this Task task, TaskCompletionSource<T> tcs)
         {
             task.ContinueWith (t => {
                 tcs.TrySetException (t.Exception.Flatten ());
